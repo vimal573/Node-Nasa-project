1576bf3b68737b8b46baedee3c23c7aa42aab54a
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-11-14 14:04:09 +0530

    DELETE launches Aborting Launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch } = require('../../models/launches.model');
+const {
+  getAllLaunches,
+  addNewLaunch,
+  existLaunchWithId,
+  abortLaunchById,
+} = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
@@ -29,7 +34,23 @@ function httpAddNewLaunch(req, res) {
   return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+  const launchId = Number(req.params.id);
+
+  // if launch doesn't exist
+  if (!existLaunchWithId(launchId)) {
+    return res.status(404).json({
+      error: 'Launch not found',
+    });
+  }
+
+  // if launch does exist
+  const aborted = abortLaunchById(launchId);
+  return res.status(200).json(aborted);
+}
+
 module.exports = {
   httpGetAllLaunches,
   httpAddNewLaunch,
+  httpAbortLaunch,
 };
