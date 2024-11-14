9ced3e0338d500d482be11a33dd2967c3881fdfa
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-11-14 15:13:53 +0530

    Testing API Endpoints With Supertest POST

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -5,13 +5,38 @@ describe('Test GET /launches', () => {
   test('It should respond with 200 success', async () => {
     const response = await request(app)
       .get('/launches')
-      .expect('content-Type', /json/)
+      .expect('Content-Type', /json/)
       .expect(200);
   });
 });
 
 describe('Test POST /launches', () => {
-  test('It should respond with 200 success', () => {});
+  const completeLaunchData = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-186 f',
+    launchDate: 'January 4, 2028',
+  };
+
+  const launchDataWithoutData = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-186 f',
+  };
+
+  test('It should respond with 201 created', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(completeLaunchData)
+      .expect('Content-Type', /json/)
+      .expect(201);
+
+    const requestdate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(responseDate).toBe(requestdate);
+
+    expect(response.body).toMatchObject(launchDataWithoutData);
+  });
 
   test('It should catch missing required properties', () => {});
 
