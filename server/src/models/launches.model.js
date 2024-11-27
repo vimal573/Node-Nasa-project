0a0338e4402234629ef898e545716e48e9774297
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-11-27 21:56:50 +0530

    Scheduling New Launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -60,18 +60,31 @@ async function saveLaunch(launch) {
   );
 }
 
-function addNewLaunch(launch) {
-  latestFlightNumber++;
-  launches.set(
-    latestFlightNumber,
-    Object.assign(launch, {
-      success: true,
-      upcoming: true,
-      customers: ['Zero to Mastery', 'NASA'],
-      flightNumber: latestFlightNumber,
-    })
-  );
+async function schedueNewLaunch(launch) {
+  const newFlightNumber = (await getLatestFlightNumber()) + 1;
+
+  const newLaunch = Object.assign(launch, {
+    success: true,
+    upcoming: true,
+    customers: ['Zero to Mastery', 'NASA'],
+    flightNumber: newFlightNumber,
+  });
+
+  await saveLaunch(newLaunch);
 }
+
+// function addNewLaunch(launch) {
+//   latestFlightNumber++;
+//   launches.set(
+//     latestFlightNumber,
+//     Object.assign(launch, {
+//       success: true,
+//       upcoming: true,
+//       customers: ['Zero to Mastery', 'NASA'],
+//       flightNumber: latestFlightNumber,
+//     })
+//   );
+// }
 
 function abortLaunchById(launchId) {
   const aborted = launches.get(launchId);
@@ -83,6 +96,6 @@ function abortLaunchById(launchId) {
 module.exports = {
   existLaunchWithId,
   getAllLaunches,
-  addNewLaunch,
+  schedueNewLaunch,
   abortLaunchById,
 };
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,6 +1,6 @@
 const {
   getAllLaunches,
-  addNewLaunch,
+  schedueNewLaunch,
   existLaunchWithId,
   abortLaunchById,
 } = require('../../models/launches.model');
@@ -9,7 +9,7 @@ async function httpGetAllLaunches(req, res) {
   return res.status(200).json(await getAllLaunches());
 }
 
-function httpAddNewLaunch(req, res) {
+async function httpAddNewLaunch(req, res) {
   const launch = req.body;
 
   if (
@@ -30,7 +30,7 @@ function httpAddNewLaunch(req, res) {
     });
   }
 
-  addNewLaunch(launch);
+  await schedueNewLaunch(launch);
   return res.status(201).json(launch);
 }
 
